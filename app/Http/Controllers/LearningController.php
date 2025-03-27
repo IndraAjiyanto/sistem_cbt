5ebb155cea708a85a5cac6125f7b10b713708998
IndraAjiyanto <indraajiyanto052@gmail.com>
2025-03-27 10:55:25 +0700

    rapport students

--- app/Http/Controllers/LearningController.php
+++ app/Http/Controllers/LearningController.php
@@ -47,4 +47,22 @@ class LearningController extends Controller
             'question' => $currentQuestion
         ]);
     }
+
+    public function learning_rapport(Course $course){
+        $userId = Auth::id();
+        $studentAnswers = StudentAnswer::with('question')->whereHas('question', function ($query) use ($course){
+            $query->where('course_id', $course->id);
+        })->where('user_id', $userId)->get();
+
+        $totalQuestions = CourseQuestion::where('course_id', $course->id)->count();
+        $correctAnswersCount = $studentAnswers->where('answer', 'correct')->count();
+        $passed = $correctAnswersCount == $totalQuestions;
+        return view('student.courses.learning_raport',[
+            'course' => $course,
+            'passed' => $passed,
+            'studentAnswers' => $studentAnswers,
+            'totalQuestions' => $totalQuestions,
+            'correctAnswersCount' => $correctAnswersCount,
+        ]);
+    }
 }
